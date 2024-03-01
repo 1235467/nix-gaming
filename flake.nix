0452a4f01b9f47eb7ab1151b2aeaaec520b7b31b
hakutaku <qq20099@outlook.com>
2024-03-01 12:50:17 +0800

    update to lastest

--- flake.nix
+++ flake.nix
@@ -31,9 +31,4 @@
       };
     };
 
-  # auto-fetch deps when `nix run/shell`ing
-  nixConfig = {
-    extra-substituters = ["https://nix-gaming.cachix.org"];
-    extra-trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
-  };
 }
